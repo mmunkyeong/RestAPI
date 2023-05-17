2ff8df61979074d5ffd7e30c5708b9b173bee045
mmunkyeong <jmk4379@naver.com>
2023-05-17 15:37:14 +0900

    JPA add

--- src/main/java/com/ll/restproject/RestProjectApplication.java
+++ src/main/java/com/ll/restproject/RestProjectApplication.java
@@ -2,8 +2,10 @@ package com.ll.restproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RestProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ll/restproject/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/restproject/boundedContext/member/controller/MemberController.java
@@ -1,14 +1,36 @@
 package com.ll.restproject.boundedContext.member.controller;
 
+import com.ll.restproject.boundedContext.member.entity.Member;
+import com.ll.restproject.boundedContext.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/member")
+@RequestMapping(value="/member", produces=APPLICATION_JSON_VALUE, consumes=APPLICATION_JSON_VALUE)
 public class MemberController {
-    @PostMapping("/login")
-    public String login() {
-        return "성공";
+    private final MemberService memberService;
+    @Data
+    public static class LoginRequest{
+        @NotBlank
+        private String username;
+
+        @NotBlank
+        private String password;
+    }
+    @PostMapping("/login") //@RequestBody json으로 받은 데이터를 해석할 수 있음
+    public Member login(@Valid @RequestBody LoginRequest loginRequest) {
+        return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
     }
 }
