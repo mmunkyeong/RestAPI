8c4cdd2e750f280688d1ab6bb68f601966d8ea3d
mmunkyeong <jmk4379@naver.com>
2023-05-18 10:26:03 +0900

    member/me

--- src/main/java/com/ll/restproject/boundedContext/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/restproject/boundedContext/member/controller/ApiV1MemberController.java
@@ -1,22 +1,19 @@
 package com.ll.restproject.boundedContext.member.controller;
 
 import com.ll.restproject.base.rsData.RsData;
+import com.ll.restproject.boundedContext.member.entity.Member;
 import com.ll.restproject.boundedContext.member.service.MemberService;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.util.MimeTypeUtils.ALL_VALUE;
 
 
 @RequiredArgsConstructor
@@ -40,13 +37,31 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("/login") //@RequestBody json으로 받은 데이터를 해석할 수 있음
-    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
        String accessToken=memberService.genAccessToken(loginRequest.getUsername(),loginRequest.getPassword());
 
         return RsData.of(
                 "S-1",
                 "엑세스토큰이 생성되었습니다.",
                 new LoginResponse(accessToken)
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MeResponse {
+        private final Member member;
+    }
+
+    // consumes = ALL_VALUE => 나는 딱히 JSON 을 입력받기를 고집하지 않겠다.
+    @GetMapping(value = "/me", consumes = ALL_VALUE)
+    public RsData<MeResponse> me() {
+        Member member = memberService.findByUsername("user1").get();
+
+        return RsData.of(
+                "S-1",
+                "성공",
+                new MeResponse(member)
         );
     }
 }
