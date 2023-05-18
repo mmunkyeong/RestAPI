1ea18d470a7378d53c9ca1cdb0bbb04cba7fcb38
mmunkyeong <jmk4379@naver.com>
2023-05-18 11:46:03 +0900

    test success

---
+++ src/main/java/com/ll/restproject/base/security/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.ll.restproject.base.security.service;
+
+
+import com.ll.restproject.boundedContext.member.entity.Member;
+import com.ll.restproject.boundedContext.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new User(member.getUsername(), member.getPassword(), member.getAuthorities());
+    }
+}
