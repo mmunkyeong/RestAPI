521605f1a62ffa4b09af81e8269006ff5b047383
mmunkyeong <jmk4379@naver.com>
2023-05-18 14:24:40 +0900

    스웨거에서 로그인 기능구현

--- src/main/java/com/ll/restproject/boundedContext/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/restproject/boundedContext/member/controller/ApiV1MemberController.java
@@ -4,6 +4,7 @@ import com.ll.restproject.base.rsData.RsData;
 import com.ll.restproject.boundedContext.member.entity.Member;
 import com.ll.restproject.boundedContext.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -61,7 +62,7 @@ public class ApiV1MemberController {
 
     // consumes = ALL_VALUE => 나는 딱히 JSON 을 입력받기를 고집하지 않겠다.
     @GetMapping(value = "/me", consumes = ALL_VALUE)
-    @Operation(summary = "로그인된 사용자의 정보")
+    @Operation(summary = "로그인된 사용자의 정보",security = @SecurityRequirement(name = "bearerAuth"))
     public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
         Member member = memberService.findByUsername(user.getUsername()).get();
 
